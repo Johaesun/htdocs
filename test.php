6cf0eb527566cdd7388d2ac146525e3a415975ca
Johaesun <991015a@naver.com>
2016-09-01 11:36:23 +0900

    구구단완료

--- test.php
+++ test.php
@@ -6,6 +6,11 @@
 <head>
     <meta charset="UTF-8">
     <title>혜선님이시다~!</title>
+    <style>
+        ._99{background: pink;}
+        .one{color:blue;}
+        .two{color:red;}
+    </style>
 </head>
 <body>
 <h1>10!15!</h1>
@@ -33,6 +38,63 @@
         
         print __DIR__;
         ?></li>
-</ul>    
+    <li><?php
+        $data="안뇽";
+        $num=4;
+        print $data;
+        print $num;
+       ?>
+       </li>
+       
+    <li>
+    <?php
+    $week[]="월";
+    $week[]="월";
+    //위의 코드와 같은 의미입니다.
+    $week[0]="월";
+    $weep[1]="화";
+    print $week[0];
+    print $week[1];
+        
+        /*array 함수 사용하기*/
+        //5.4이상부터 사용가능
+        $month=array(18,18,18,18,18,18,"신발");
+        $year=[2016,2017];
+        
+        //배열초기호ㅏ
+        $month=array();
+        
+        //연관배열
+        $member["name"]="꽉일촌";
+        $member["age"]=3;
+        $member["height"]=150;
+        print $member["age"];
+        print "<pre>";
+        print_r ($member);
+        
+        //for문
+        
+        ?>
+        
+    </li>
+    </ul> 
+    
+    <p class="_99">
+    <?php
+    for($i=1;$i<10;$i++){
+            print "<br>";
+        if($i%2!=1){
+        for($j=1;$j<10;$j++){
+            print "<span class='one'>".$i."X".$j."=".$i*$j."<br></span>";
+                }
+        }
+        else{
+            for($j=1;$j<10;$j++){
+            print "<span class='two'>".$i."X".$j."=".$i*$j."<br></span>";
+                }
+            }   
+        }
+      ?>
+      </p>
 </body>
 </html>
