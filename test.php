30f5532d7a971d7275de8f7167c4dcf65701cd40
Johaesun <991015a@naver.com>
2016-09-01 10:25:48 +0900

    혜선님이시다

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+    phpinfo();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>혜선님이시다~!</title>
+</head>
+<body>
+<h1>10!15!</h1>
+<ul>
+    <li>룰</li>
+    <li>루</li>
+    
+<?php
+    print "안녕이시당 혜선님이시당 10월 15일 생일이시당 숭구리당당 숭당당";
+    ?>
+    
+    <li>랄</li>
+    <li>라</li>
+</ul>    
+</body>
+</html>
